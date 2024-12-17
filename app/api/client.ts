ef2574b331ff0c3ed40f55d4df4b323f2b18e75c
Natanael <natanael_magalhaes@msn.com>
2024-12-17 03:20:41 -0300

    https

--- app/api/client.ts
+++ app/api/client.ts
@@ -4,7 +4,7 @@ import axios from 'axios'
 
 let baseUrl
 if (process.env.NODE_ENV === 'production') {
-  baseUrl = 'http://35.209.139.248/'
+  baseUrl = 'https://35.209.139.248/'
 } else {
   baseUrl = 'http://localhost:44777'
 }
